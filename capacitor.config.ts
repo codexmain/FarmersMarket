1b3415758a32921cacd8ac44135a6b9ebe092818
Ignacio Fuenzalida <i.fuenzalida@duocuc.cl>
2024-08-19 20:11:46 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FarmersMarket',
+  webDir: 'www'
+};
+
+export default config;
