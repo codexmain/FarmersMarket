1dcabee12040a5aa7dcccd1ee561f5602f337c8d
Ignacio Fuenzalida <i.fuenzalida@duocuc.cl>
2024-08-24 17:48:26 -0400

    Avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inicio', url: '/folder/Inicio', icon: 'home' },
-    { title: 'Productos', url: '/folder/Productos', icon: 'bag' },
+    { title: 'Inicio', url: 'inicio', icon: 'home' },
+    { title: 'Productos', url: 'productos', icon: 'bag' },
     { title: 'Favoritos', url: '/folder/Favoritos', icon: 'heart' },
     { title: 'Mis Compras', url: '/folder/Mis Compras', icon: 'pricetags' },
     { title: 'Carrito', url: '/folder/Carrito', icon: 'cart' },
     { title: 'Mi Cuenta', url: '/folder/Mi Cuenta', icon: 'person' },
-    { title: 'Login', url: 'login', icon: 'log-in' },
+    { title: 'Salir de la cuenta', url: 'login', icon: 'log-out' },
   ];
   public labels = ['Promociones', 'Descuentos'];
   constructor() {}
