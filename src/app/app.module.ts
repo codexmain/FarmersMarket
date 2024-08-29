08a2b5c754427597b7769b7bac28ff945856e7ba
vicente <vicerivera.a@gmail.com>
2024-08-29 16:37:12 -0400

    Integracion Login/Recuperar Password
    Se incorporó el login con el recuperar contraseña con Ion Modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+//import { RecuperarPasswordPage } from './users/recuperar-password/recuperar-password.page';
 
 @NgModule({
   declarations: [AppComponent],
