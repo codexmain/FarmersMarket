c17f92013c81bc665fb27a36511e2641ec2f83f6
vicente <vicerivera.a@gmail.com>
2024-08-30 00:57:49 -0400

    30/08/24 Avanze del uso de navigations extras
    Se intento avanzar en la transferencia de datos, no con mucho exito ya que en clases no se vio como transferir arreglos o transferir a multiples ventanas

--- src/app/administracion/admin-page/admin-page.page.ts
+++ src/app/administracion/admin-page/admin-page.page.ts
@@ -1,24 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import {  MenuController, } from '@ionic/angular';
 
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.page.html',
   styleUrls: ['./admin-page.page.scss'],
 })
-export class AdminPagePage implements OnInit {
+export class AdminPagePage implements OnInit {  //! es obligatoria, ? es opcional
+  pfp?: string; //aca se va a ocupar el path de la imagen
+  primerNombre!: string;
+  segundoNombre?: string;
+  aPaterno!: string;
+  aMaterno?: string;
+  correo!: string;
+  clave!: string;
+  empresa?: string;
+  estadoUser!: string;
+  tipoUser!: number;
+  region!: string;
+  comuna!: string;
+  direccion!: string;
 
-  constructor(private menu: MenuController) { }
+
+
+  constructor(private router: Router, private activerouter: ActivatedRoute) {
+    this.activerouter.queryParams.subscribe(param =>{
+      //valido si en la navegación existe la variable de contexto
+      if(this.router.getCurrentNavigation()?.extras.state){
+        this.pfp = this.router.getCurrentNavigation()?.extras?.state?.['perfil'];
+        this.primerNombre = this.router.getCurrentNavigation()?.extras?.state?.['fstName'];
+        this.segundoNombre = this.router.getCurrentNavigation()?.extras?.state?.['sndName'];
+        this.aPaterno = this.router.getCurrentNavigation()?.extras?.state?.['fstSurname'];
+        this.aMaterno = this.router.getCurrentNavigation()?.extras?.state?.['sndSurname'];
+        this.correo = this.router.getCurrentNavigation()?.extras?.state?.['mail'];
+        this.clave= this.router.getCurrentNavigation()?.extras?.state?.['pwd'];
+        this.empresa = this.router.getCurrentNavigation()?.extras?.state?.['company'];
+        this.estadoUser = this.router.getCurrentNavigation()?.extras?.state?.['uStatus'];
+        this.tipoUser = this.router.getCurrentNavigation()?.extras?.state?.['userType'];
+        this.region = this.router.getCurrentNavigation()?.extras?.state?.['reg'];
+        this.comuna = this.router.getCurrentNavigation()?.extras?.state?.['com'];
+        this.direccion = this.router.getCurrentNavigation()?.extras?.state?.['loc'];
+      }
+    })
+
+   }
+
 
   ngOnInit() {
   }
 
-  ionViewWillEnter() {
-    this.menu.enable(false);
-  }
-
-  ionViewWillLeave() {
-    this.menu.enable(true);
-  }
 
 }
--- src/app/cliente/cuenta/cuenta.page.ts
+++ src/app/cliente/cuenta/cuenta.page.ts
@@ -1,15 +1,25 @@
+import { ɵnormalizeQueryParams } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-cuenta',
   templateUrl: './cuenta.page.html',
   styleUrls: ['./cuenta.page.scss'],
 })
 export class CuentaPage implements OnInit {
+  userData: any;
 
-  constructor() { }
+
+  constructor(private route: Router, private activerouter: ActivatedRoute) {
+    this.userData = this.route.getCurrentNavigation()?.extras?.state;
+    
+  }
+
 
   ngOnInit() {
   }
+
+
+
 
 }
