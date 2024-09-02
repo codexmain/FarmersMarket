c353e3f74530259f7187bac041738795e9ab7862
vicente <vicerivera.a@gmail.com>
2024-09-01 23:02:10 -0400

    Correccion despliege de informacion en Gestion mi tienda
    Despliege de informacion en el html usuario (esto para sellers,  en Gestion mi tienda)

--- src/app/vendedor/vendedor-page/vendedor-page.page.ts
+++ src/app/vendedor/vendedor-page/vendedor-page.page.ts
@@ -7,72 +7,34 @@ import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
   styleUrls: ['./vendedor-page.page.scss'],
 })
 export class VendedorPagePage implements OnInit {//! es obligatoria, ? es opcional
-  pfp?: string; //aca se va a ocupar el path de la imagen
-  primerNombre!: string;
-  segundoNombre?: string;
-  aPaterno!: string;
-  aMaterno?: string;
-  correo!: string;
-  clave!: string;
-  empresa?: string;
-  estadoUser!: string;
-  tipoUser!: number;
-  region!: string;
-  comuna!: string;
-  direccion!: string;
-  emails: string[] = []; //recibimiento de todos los email para validaciones, desde el login 
+  userData: any;
 
-  constructor(private router: Router, private activerouter: ActivatedRoute) {
-    this.activerouter.queryParams.subscribe(param => {
-      //valido si en la navegación existe la variable de contexto
-      if (this.router.getCurrentNavigation()?.extras.state) {
-        this.pfp = this.router.getCurrentNavigation()?.extras?.state?.['perfil'];
-        this.primerNombre = this.router.getCurrentNavigation()?.extras?.state?.['fstName'];
-        this.segundoNombre = this.router.getCurrentNavigation()?.extras?.state?.['sndName'];
-        this.aPaterno = this.router.getCurrentNavigation()?.extras?.state?.['fstSurname'];
-        this.aMaterno = this.router.getCurrentNavigation()?.extras?.state?.['sndSurname'];
-        this.correo = this.router.getCurrentNavigation()?.extras?.state?.['mail'];
-        this.clave = this.router.getCurrentNavigation()?.extras?.state?.['pwd'];
-        this.empresa = this.router.getCurrentNavigation()?.extras?.state?.['company'];
-        this.estadoUser = this.router.getCurrentNavigation()?.extras?.state?.['uStatus'];
-        this.tipoUser = this.router.getCurrentNavigation()?.extras?.state?.['userType'];
-        this.region = this.router.getCurrentNavigation()?.extras?.state?.['reg'];
-        this.comuna = this.router.getCurrentNavigation()?.extras?.state?.['com'];
-        this.direccion = this.router.getCurrentNavigation()?.extras?.state?.['loc'];
-        this.emails = this.router.getCurrentNavigation()?.extras?.state?.['emails']; //recibir array de correos
-      }
-    })
-
+  constructor(private route: Router, private activerouter: ActivatedRoute) { 
+    this.userData = this.route.getCurrentNavigation()?.extras?.state;
   }
   ngOnInit() {
-    console.log(this.emails);
+    
   }
 
   navigateToUsuario() { //transferencia de array de correos a la parte de usuario
-    let navigationExtras: NavigationExtras = {
-      state: {
-        emails: this.emails
-      }
+    let navigationextras: NavigationExtras = {
+      state: this.userData
     };
-    this.router.navigate(['/usuario'], navigationExtras);
+    this.route.navigate(['/usuario'], navigationextras);
   }
 
   navigateToProventas() { //transferencia de array de correos a la parte de proventas
-    let navigationExtras: NavigationExtras = {
-      state: {
-        emails: this.emails
-      }
+    let navigationextras: NavigationExtras = {
+      state: this.userData
     };
-    this.router.navigate(['/proventas'], navigationExtras);
+    this.route.navigate(['/proventas'], navigationextras);
   }
 
   navigateToRegventas() { //transferencia de array de correos a la parte de regventas
-    let navigationExtras: NavigationExtras = {
-      state: {
-        emails: this.emails
-      }
+    let navigationextras: NavigationExtras = {
+      state: this.userData
     };
-    this.router.navigate(['/regventas'], navigationExtras);
+    this.route.navigate(['/regventas'], navigationextras);
   }
 
 }
