bc34d6b91266a103d33617c0aad32f9c93d259c5
vicente <vicerivera.a@gmail.com>
2024-09-29 15:40:56 -0300

    Implementaciones Basicas en Category en Admin Page
    Se realiaron cosas simplres, realizar el html basico de la vista general de Categoría, junto con el formulario de agregar (junto con sus validaciones )
    
    Nota: Falta conectarlo a la base de datos

--- src/app/administracion/add-categoria/add-categoria.page.ts
+++ src/app/administracion/add-categoria/add-categoria.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController, ModalController, AlertController, NavParams } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-categoria',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCategoriaPage implements OnInit {
 
-  constructor() { }
+  constructor(private modalController: ModalController, private menu: MenuController, private route: ActivatedRoute, private router: Router, public alertController: AlertController, private navParams: NavParams) { 
+  }
+
+  //inputs del formulario
+  categoryName: string = '';
 
   ngOnInit() {
+  }
+
+  async presentAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+
+  async agregarCategoria(){
+          // Validar contraseña
+
+  const categoryPattern = /^[a-zA-Z0-9\s]{5,50}$/;
+  if (this.categoryName && !categoryPattern.test(this.categoryName)) {
+    this.presentAlert('Error', 'El nombre de la categoría debe tener entre 5 y 50 caracteres. Y solo debe contener letras, números y espacios');
+    return;
+  }
+
+    // Si todas las validaciones pasan
+    this.presentAlert('Éxito', 'Se ha agregado la Categoría exitosamente.');
+    console.log('Formulario válido, proceder con el registro.');
+    this.modalController.dismiss();
+
+  };
+
+  dismiss() {
+    this.modalController.dismiss();
   }
 
 }
