4ba7235e4cd7fad269d6004b051af102abb0bd90
vicente <vicerivera.a@gmail.com>
2024-09-29 23:53:32 -0300

    Avanze, menu Productos y agregar productos
    en productos falta hacer la validacion de formatos y realizar la conexion con la bbdd

--- src/app/administracion/add-item/add-item.page.ts
+++ src/app/administracion/add-item/add-item.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController, ModalController, AlertController, NavParams } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-add-item',
@@ -7,9 +10,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddItemPage implements OnInit {
 
-  constructor() { }
+  productName: string = '';
+  descProduct: string = '';
+  productPrice!: number;
+  stock: number = 0;
+  isOrganic: boolean = false;
+  seller!: number;
+  category!: number;
+  subCategory!: number;
+  photo: string = ''; // Ruta de la foto
+
+  constructor(private modalController: ModalController, private menu: MenuController, private route: ActivatedRoute, private router: Router, public alertController: AlertController, private navParams: NavParams) { }
+
+
 
   ngOnInit() {
   }
+  async presentAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['OK']
+    });
 
-}
+    await alert.present();
+  }
+
+  async agregarProducto(){ 
+
+    if (!this.seller) {
+      this.presentAlert('Error', 'El Vendedor es un campo obligatorio.');
+      return;}
+
+    if (!this.productName) {
+      this.presentAlert('Error', 'El Nombre del Producto es un campo obligatorio.');
+      return;}
+
+
+    if (!this.productPrice) {
+      this.presentAlert('Error', 'El Precio del producto es un campo obligatorio.');
+      return;}
+
+    console.log('Valor de productStock:', this.stock);
+    if (this.stock === null || this.stock === undefined) {
+      this.presentAlert('Error', 'El Stock/Existencias es un campo obligatorio.');
+      return;
+    }
+
+    if (!this.category) {
+      this.presentAlert('Error', 'La Categoría es un campo obligatorio.');
+      return;}
+      
+    if (!this.subCategory) {
+      this.presentAlert('Error', 'La Subcategoría es un campo obligatorio.');
+      return;}    
+      
+    
+
+
+  }
+
+
+  async takePhoto() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Prompt // Permite al usuario elegir entre cámara y galería
+    });
+
+    this.photo = image.webPath || ''; 
+  }
+
+
+
+
+
+
+
+
+  dismiss() {
+    this.modalController.dismiss();
+  }
+
+  
+  
+  }
