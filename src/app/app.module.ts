ca1e0834acfc72a68107374c5958c83fe7242805
vicente <vicerivera.a@gmail.com>
2024-10-04 23:36:13 -0300

    Implementacion libreria native Storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
 //import { RecuperarPasswordPage } from './users/recuperar-password/recuperar-password.page';
 
 import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx'; //sql
@@ -14,7 +15,7 @@ import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx'; //sq
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync(),SQLite],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync(),SQLite, NativeStorage],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
