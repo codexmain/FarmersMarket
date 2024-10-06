1046e9cf3e92a955d45e0cb5676758255eba1d21
vicente <vicerivera.a@gmail.com>
2024-10-06 10:35:26 -0300

    Implementacion basica CRUD y clases

---
+++ src/app/services/cmb-proveedores.spec.ts
@@ -0,0 +1,7 @@
+import { CmbProveedores } from './cmb-proveedores';
+
+describe('CmbProveedores', () => {
+  it('should create an instance', () => {
+    expect(new CmbProveedores()).toBeTruthy();
+  });
+});
