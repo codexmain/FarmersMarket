e610744702a5f8da0cf6ebe3e8ad769c367c178f
vicente <vicerivera.a@gmail.com>
2024-10-07 07:32:24 -0300

    Avanze CRUD administracion

--- src/app/services/categorias.ts
+++ src/app/services/categorias.ts
@@ -1,2 +1,4 @@
 export class Categorias {
+    id!: number;
+    nombre!: string;
 }
--- src/app/services/usuarios.ts
+++ src/app/services/usuarios.ts
@@ -1,2 +1,15 @@
 export class Usuarios {
+    id!: number;
+    nombre!: string;
+    segundo_nombre?: string;
+    apellido_paterno!: string;
+    apellido_materno?: string;
+    email!: string;
+    contrasena!: string;
+    nombre_empresa?: string;
+    descripcion_corta?: string;
+    foto_perfil?: string;
+    estado_cuenta!: string;
+    fecha_registro?: string;
+    tipo_usuario_id!: number;
 }
