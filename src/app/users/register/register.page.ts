bf45ab0db00ef09bd44435d91913b784c0608029
Ignacio Fuenzalida <i.fuenzalida@duocuc.cl>
2024-10-06 23:54:25 -0300

    Primera parte de 3
    Falta modulo inicial, cliente y vendedor(pendiente)

--- src/app/users/register/register.page.ts
+++ src/app/users/register/register.page.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController, ModalController, AlertController } from '@ionic/angular';
+import {
+  MenuController,
+  ModalController,
+  AlertController,
+} from '@ionic/angular';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataBaseService } from 'src/app/services/data-base.service';
-interface RegionesComunas { //permite hacer la dependencia de comuna / region, para que sea interactivo cada vez que se cambie la region, se vean diferentes comunas
+//usar para camara
+import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+interface RegionesComunas {
+  //permite hacer la dependencia de comuna / region, para que sea interactivo cada vez que se cambie la region, se vean diferentes comunas
   [key: string]: string[];
 }
 @Component({
@@ -11,11 +19,20 @@ interface RegionesComunas { //permite hacer la dependencia de comuna / region, p
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-
-  constructor(private modalController: ModalController, private menu: MenuController, private route: ActivatedRoute, private router: Router, public alertController: AlertController, private DataBase: DataBaseService) {
+  constructor(
+    private modalController: ModalController,
+    private menu: MenuController,
+    private route: ActivatedRoute,
+    private router: Router,
+    public alertController: AlertController,
+    private DataBase: DataBaseService,
+    //usar para camara
+    private camera: Camera,
+    private nativeStorage: NativeStorage
+  ) {
     const navigation = this.router.getCurrentNavigation();
-    if (navigation?.extras?.state) {  //recibir de login el array de correos
+    if (navigation?.extras?.state) {
+      //recibir de login el array de correos
       this.emails = navigation.extras.state['emails'];
     }
   }
@@ -30,14 +47,10 @@ export class RegisterPage implements OnInit {
   region: string = '';
   comuna: string = '';
   direccion: string = '';
-
-
-
-
+  Imagen: string = '';
 
   selectedRegion: string = '';
   comunas: string[] = [];
-
 
   regionesComunas: RegionesComunas = {
     arica: ['Arica', 'Camarones'],
@@ -55,9 +68,31 @@ export class RegisterPage implements OnInit {
     rios: ['Valdivia', 'La Unión'],
     lagos: ['Puerto Montt', 'Osorno'],
     aysen: ['Coyhaique', 'Puerto Aysén'],
-    magallanes: ['Punta Arenas', 'Puerto Natales']
+    magallanes: ['Punta Arenas', 'Puerto Natales'],
   };
 
+  // metodo para tomar la foto
+  tomarFoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+    };
+
+    this.camera.getPicture(options).then(
+      (imageData) => {
+        this.Imagen = 'data:image/jpeg;base64,' + imageData;
+        this.nativeStorage
+          .setItem('Imagen', { image: this.Imagen })
+          .then(() => console.log('Imagen guardada en Native Storage'))
+          .catch((error) => console.error('Error al guardar imagen', JSON.stringify(error)));
+      },
+      (err) => {
+        console.error('Error al tomar foto', JSON.stringify(err));
+      }
+    );
+  }
 
   onRegionChange(event: any) {
     this.selectedRegion = event.detail.value;
@@ -77,7 +112,7 @@ export class RegisterPage implements OnInit {
     console.log(this.emails);
   }
 
-  registrarUsuarios(){
+  registrarUsuarios() {
     this.DataBase.registrarUsuarios(
       this.pNombre,
       this.sNombre,
@@ -96,7 +131,7 @@ export class RegisterPage implements OnInit {
     const alert = await this.alertController.create({
       header: header,
       message: message,
-      buttons: ['OK']
+      buttons: ['OK'],
     });
 
     await alert.present();
@@ -106,16 +141,21 @@ export class RegisterPage implements OnInit {
     // Validar pNombre
 
     if (!this.pNombre || this.pNombre.length < 2) {
-      this.presentAlert('Error', 'El primer nombre es obligatorio y debe tener al menos 2 caracteres.');
+      this.presentAlert(
+        'Error',
+        'El primer nombre es obligatorio y debe tener al menos 2 caracteres.'
+      );
       return;
     }
 
     // Validar aPaterno
     if (!this.aPaterno || this.aPaterno.length < 2) {
-      this.presentAlert('Error', 'El apellido paterno es obligatorio y debe tener al menos 2 caracteres.');
+      this.presentAlert(
+        'Error',
+        'El apellido paterno es obligatorio y debe tener al menos 2 caracteres.'
+      );
       return;
     }
-
 
     // Validar email
     if (!this.email) {
@@ -137,17 +177,26 @@ export class RegisterPage implements OnInit {
 
     // Validar pNombre, sNombre, aPaterno, aMaterno
     const namePattern = /^[a-zA-Z\s]{2,}$/;
-    if (!namePattern.test(this.pNombre) || !namePattern.test(this.aPaterno) ||
+    if (
+      !namePattern.test(this.pNombre) ||
+      !namePattern.test(this.aPaterno) ||
       (this.sNombre && !namePattern.test(this.sNombre)) ||
-      (this.aMaterno && !namePattern.test(this.aMaterno))) {
-      this.presentAlert('Error', 'Los nombres y apellidos deben tener al menos 2 caracteres y no contener números.');
+      (this.aMaterno && !namePattern.test(this.aMaterno))
+    ) {
+      this.presentAlert(
+        'Error',
+        'Los nombres y apellidos deben tener al menos 2 caracteres y no contener números.'
+      );
       return;
     }
 
     // Validar empresa
     const empresaPattern = /^[a-zA-Z0-9\s]{3,}$/;
     if (this.empresa && !empresaPattern.test(this.empresa)) {
-      this.presentAlert('Error', 'El nombre de la empresa debe tener al menos 3 caracteres y solo puede contener letras, números y espacios.');
+      this.presentAlert(
+        'Error',
+        'El nombre de la empresa debe tener al menos 3 caracteres y solo puede contener letras, números y espacios.'
+      );
       return;
     }
 
@@ -158,36 +207,53 @@ export class RegisterPage implements OnInit {
       return;
     }
     if (this.emails.includes(this.email)) {
-      this.presentAlert('Error', 'El email ingresado ya está asociado a otra cuenta.');
+      this.presentAlert(
+        'Error',
+        'El email ingresado ya está asociado a otra cuenta.'
+      );
       return;
     }
-
 
     // Validar dirección
     const direccionPattern = /^[a-zA-Z0-9\s]+$/;
     if (!direccionPattern.test(this.direccion)) {
-      this.presentAlert('Error', 'La dirección solo puede contener letras, números y espacios.');
+      this.presentAlert(
+        'Error',
+        'La dirección solo puede contener letras, números y espacios.'
+      );
       return;
     }
 
     // Validar contraseña
     if (this.password.length < 10 || this.password.length > 30) {
-      this.presentAlert('Error', 'La contraseña debe tener entre 10 y 30 caracteres.');
+      this.presentAlert(
+        'Error',
+        'La contraseña debe tener entre 10 y 30 caracteres.'
+      );
       return;
     }
 
     if (!/[!@#$%^&*(),.?":{}|<>]/.test(this.password)) {
-      this.presentAlert('Error', 'La contraseña debe contener al menos un carácter especial.');
+      this.presentAlert(
+        'Error',
+        'La contraseña debe contener al menos un carácter especial.'
+      );
       return;
     }
 
     if (/(\d)\1/.test(this.password) || /([a-zA-Z])\1/.test(this.password)) {
-      this.presentAlert('Error', 'La contraseña no debe contener caracteres o números consecutivos repetidos.');
+      this.presentAlert(
+        'Error',
+        'La contraseña no debe contener caracteres o números consecutivos repetidos.'
+      );
       return;
     }
 
     if (!/(?=.*[A-Z].*[A-Z])/.test(this.password)) {
-      this.presentAlert('Error', 'La contraseña debe contener al menos dos letras mayúsculas.');
+      this.presentAlert(
+        'Error',
+        'La contraseña debe contener al menos dos letras mayúsculas.'
+      );
       return;
     }
 
@@ -201,6 +267,3 @@ export class RegisterPage implements OnInit {
     this.modalController.dismiss();
   }
 }
-
-
-
