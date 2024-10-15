08edc6ecf7f347ac241bf88c506cb762419b6fcf
vicente <vicerivera.a@gmail.com>
2024-10-15 20:17:44 -0300

    avances google maps

--- src/app/administracion/add-usuarios/add-usuarios.module.ts
+++ src/app/administracion/add-usuarios/add-usuarios.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,12 +10,15 @@ import { AddUsuariosPageRoutingModule } from './add-usuarios-routing.module';
 import { AddUsuariosPage } from './add-usuarios.page';
 
 @NgModule({
+  
+
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
     AddUsuariosPageRoutingModule
   ],
-  declarations: [AddUsuariosPage]
+  declarations: [AddUsuariosPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AddUsuariosPageModule {}
