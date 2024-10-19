b6995db21e51dc09d0a3fd94dbd9ff1f638297c8
Ignacio Fuenzalida <i.fuenzalida@duocuc.cl>
2024-10-19 17:34:01 -0300

    users funcional

--- src/app/users/register/register.page.ts
+++ src/app/users/register/register.page.ts
@@ -2,67 +2,184 @@ import { Component, OnInit } from '@angular/core';
 import { MenuController, ModalController, AlertController } from '@ionic/angular';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataBaseService } from 'src/app/services/data-base.service';
-interface RegionesComunas { //permite hacer la dependencia de comuna / region, para que sea interactivo cada vez que se cambie la region, se vean diferentes comunas
+import { Camera, CameraResultType } from '@capacitor/camera';
+
+interface RegionesComunas {
   [key: string]: string[];
 }
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-
-  constructor(private modalController: ModalController, private menu: MenuController, private route: ActivatedRoute, private router: Router, public alertController: AlertController, private DataBase: DataBaseService) {
-    const navigation = this.router.getCurrentNavigation();
-    if (navigation?.extras?.state) {  //recibir de login el array de correos
-      this.emails = navigation.extras.state['emails'];
-    }
-  }
-  emails: string[] = [];
+  
+  // Variables del formulario
   pNombre: string = '';
   sNombre: string = '';
   aPaterno: string = '';
   aMaterno: string = '';
   empresa: string = '';
+  descripcion_corta: string = '';
   email: string = '';
   password: string = '';
-  region: string = '';
-  comuna: string = '';
   direccion: string = '';
+  foto_perfil: string = '';
+  estado_cuenta: string = 'activa';
+  tipo_usuario_id: number = 1;
+  imagen: any;
 
+  selectedRegion: number | null = null;
+  selectedComuna: number | null = null;
+  regiones: any[] = [];
+  comunas: any[] = [];
+  
+  emails: string[] = [];
+  
 
+  constructor(
+    private modalController: ModalController,
+    private menu: MenuController,
+    private route: ActivatedRoute,
+    private router: Router,
+    public alertController: AlertController,
+    private dataBase: DataBaseService
+  ) {
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation?.extras?.state) {
+      this.emails = navigation.extras.state['emails'];
+    }
+  }
 
+  ngOnInit() {
+    this.cargarRegiones();
+  }
 
+  async cargarRegiones() {
+    try {
+      this.regiones = await this.dataBase.Regiones();
+    } catch (error) {
+      console.error('Error al cargar regiones:', error);
+    }
+  }
 
-  selectedRegion: string = '';
-  comunas: string[] = [];
-
-
-  regionesComunas: RegionesComunas = {
-    arica: ['Arica', 'Camarones'],
-    tarapaca: ['Iquique', 'Alto Hospicio'],
-    antofagasta: ['Antofagasta', 'Mejillones'],
-    atacama: ['Copiapó', 'Caldera'],
-    coquimbo: ['La Serena', 'Coquimbo'],
-    valparaiso: ['Valparaíso', 'Viña del Mar'],
-    metropolitana: ['Santiago', 'Puente Alto'],
-    ohiggins: ['Rancagua', 'San Fernando'],
-    maule: ['Talca', 'Curicó'],
-    nuble: ['Chillán', 'San Carlos'],
-    biobio: ['Concepción', 'Los Ángeles'],
-    araucania: ['Temuco', 'Villarrica'],
-    rios: ['Valdivia', 'La Unión'],
-    lagos: ['Puerto Montt', 'Osorno'],
-    aysen: ['Coyhaique', 'Puerto Aysén'],
-    magallanes: ['Punta Arenas', 'Puerto Natales']
-  };
-
-
-  onRegionChange(event: any) {
+  async cargarcomunas(event: any) {
     this.selectedRegion = event.detail.value;
-    this.comunas = this.regionesComunas[this.selectedRegion] || [];
-    this.comuna = ''; // Resetear la comuna cuando se cambia la región
+    if (this.selectedRegion) {
+      try {
+        this.comunas = await this.dataBase.Comunas(this.selectedRegion);
+      } catch (error) {
+        console.error('Error al cargar comunas:', error);
+      }
+    } else {
+      this.comunas = [];
+    }
+    this.selectedComuna = null;
+  }
+
+  async presentAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  async registrarse() {
+    if (!this.validarFormulario()) return;
+
+    const nuevoUsuario = {
+      nombre: this.pNombre,
+      segundo_nombre: this.sNombre || null,
+      apellido_paterno: this.aPaterno,
+      apellido_materno: this.aMaterno || null,
+      email: this.email,
+      contrasena: this.password,
+      nombre_empresa: this.empresa || null,
+      descripcion_corta: this.descripcion_corta || null,
+      foto_perfil: this.foto_perfil || null,
+      estado_cuenta: this.estado_cuenta,
+      tipo_usuario_id: this.tipo_usuario_id,
+      region_id: this.selectedRegion,
+      comuna_id: this.selectedComuna,
+      direccion: this.direccion,
+    };
+
+    const registroExitoso = await this.dataBase.registrarUsuario(nuevoUsuario);
+
+    if (registroExitoso) {
+      await this.presentAlert('Éxito', 'Usuario registrado exitosamente.');
+      this.router.navigate(['/login']);
+    } else {
+      await this.presentAlert('Error', 'Hubo un problema al registrar el usuario.');
+    }
+  }
+
+  validarFormulario(): boolean {
+    const namePattern = /^[a-zA-Z\s]{2,}$/;
+    const empresaPattern = /^[a-zA-Z0-9\s]{3,}$/;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const direccionPattern = /^[a-zA-Z0-9\s]+$/;
+
+    if (!this.pNombre || !namePattern.test(this.pNombre)) {
+      this.presentAlert('Error', 'El primer nombre es obligatorio y debe tener al menos 2 caracteres.');
+      return false;
+    }
+    if (!this.aPaterno || !namePattern.test(this.aPaterno)) {
+      this.presentAlert('Error', 'El apellido paterno es obligatorio y debe tener al menos 2 caracteres.');
+      return false;
+    }
+    if (this.sNombre && !namePattern.test(this.sNombre)) {
+      this.presentAlert('Error', 'El segundo nombre debe tener al menos 2 caracteres y no contener números.');
+      return false;
+    }
+    if (this.aMaterno && !namePattern.test(this.aMaterno)) {
+      this.presentAlert('Error', 'El apellido materno debe tener al menos 2 caracteres y no contener números.');
+      return false;
+    }
+    if (this.empresa && !empresaPattern.test(this.empresa)) {
+      this.presentAlert('Error', 'El nombre de la empresa debe tener al menos 3 caracteres.');
+      return false;
+    }
+    if (!emailPattern.test(this.email) || this.emails.includes(this.email)) {
+      this.presentAlert('Error', 'El email es obligatorio, debe tener un formato válido y no estar registrado.');
+      return false;
+    }
+    if (!this.selectedRegion) {
+      this.presentAlert('Error', 'La región es obligatoria.');
+      return false;
+    }
+    if (!this.selectedComuna) {
+      this.presentAlert('Error', 'La comuna es obligatoria.');
+      return false;
+    }
+    if (!direccionPattern.test(this.direccion)) {
+      this.presentAlert('Error', 'La dirección solo puede contener letras, números y espacios.');
+      return false;
+    }
+    return this.validarPassword();
+  }
+
+  validarPassword(): boolean {
+    if (this.password.length < 10 || this.password.length > 30) {
+      this.presentAlert('Error', 'La contraseña debe tener entre 10 y 30 caracteres.');
+      return false;
+    }
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(this.password)) {
+      this.presentAlert('Error', 'La contraseña debe contener al menos un carácter especial.');
+      return false;
+    }
+    if (/(\d)\1/.test(this.password) || /([a-zA-Z])\1/.test(this.password)) {
+      this.presentAlert('Error', 'La contraseña no debe tener caracteres o números consecutivos repetidos.');
+      return false;
+    }
+    if (!/(?=.*[A-Z].*[A-Z])/.test(this.password)) {
+      this.presentAlert('Error', 'La contraseña debe contener al menos dos letras mayúsculas.');
+      return false;
+    }
+    return true;
   }
 
   ionViewWillEnter() {
@@ -73,120 +190,21 @@ export class RegisterPage implements OnInit {
     this.menu.enable(true);
   }
 
-  ngOnInit() {
-    console.log(this.emails);
-  }
-
-
-  async presentAlert(header: string, message: string) {
-    const alert = await this.alertController.create({
-      header: header,
-      message: message,
-      buttons: ['OK']
-    });
-
-    await alert.present();
-  }
-
-  async registrarse() {
-    // Validar pNombre
-
-    if (!this.pNombre || this.pNombre.length < 2) {
-      this.presentAlert('Error', 'El primer nombre es obligatorio y debe tener al menos 2 caracteres.');
-      return;
-    }
-
-    // Validar aPaterno
-    if (!this.aPaterno || this.aPaterno.length < 2) {
-      this.presentAlert('Error', 'El apellido paterno es obligatorio y debe tener al menos 2 caracteres.');
-      return;
-    }
-
-
-    // Validar email
-    if (!this.email) {
-      this.presentAlert('Error', 'El email es obligatorio.');
-      return;
-    }
-
-    // Validar región
-    if (!this.region) {
-      this.presentAlert('Error', 'La región es obligatoria.');
-      return;
-    }
-
-    // Validar comuna
-    if (!this.comuna) {
-      this.presentAlert('Error', 'La comuna es obligatoria.');
-      return;
-    }
-
-    // Validar pNombre, sNombre, aPaterno, aMaterno
-    const namePattern = /^[a-zA-Z\s]{2,}$/;
-    if (!namePattern.test(this.pNombre) || !namePattern.test(this.aPaterno) ||
-      (this.sNombre && !namePattern.test(this.sNombre)) ||
-      (this.aMaterno && !namePattern.test(this.aMaterno))) {
-      this.presentAlert('Error', 'Los nombres y apellidos deben tener al menos 2 caracteres y no contener números.');
-      return;
-    }
-
-    // Validar empresa
-    const empresaPattern = /^[a-zA-Z0-9\s]{3,}$/;
-    if (this.empresa && !empresaPattern.test(this.empresa)) {
-      this.presentAlert('Error', 'El nombre de la empresa debe tener al menos 3 caracteres y solo puede contener letras, números y espacios.');
-      return;
-    }
-
-    // Validar email
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(this.email)) {
-      this.presentAlert('Error', 'El formato del email no es válido.');
-      return;
-    }
-    if (this.emails.includes(this.email)) {
-      this.presentAlert('Error', 'El email ingresado ya está asociado a otra cuenta.');
-      return;
-    }
-
-
-    // Validar dirección
-    const direccionPattern = /^[a-zA-Z0-9\s]+$/;
-    if (!direccionPattern.test(this.direccion)) {
-      this.presentAlert('Error', 'La dirección solo puede contener letras, números y espacios.');
-      return;
-    }
-
-    // Validar contraseña
-    if (this.password.length < 10 || this.password.length > 30) {
-      this.presentAlert('Error', 'La contraseña debe tener entre 10 y 30 caracteres.');
-      return;
-    }
-
-    if (!/[!@#$%^&*(),.?":{}|<>]/.test(this.password)) {
-      this.presentAlert('Error', 'La contraseña debe contener al menos un carácter especial.');
-      return;
-    }
-
-    if (/(\d)\1/.test(this.password) || /([a-zA-Z])\1/.test(this.password)) {
-      this.presentAlert('Error', 'La contraseña no debe contener caracteres o números consecutivos repetidos.');
-      return;
-    }
-
-    if (!/(?=.*[A-Z].*[A-Z])/.test(this.password)) {
-      this.presentAlert('Error', 'La contraseña debe contener al menos dos letras mayúsculas.');
-      return;
-    }
-
-    // Si todas las validaciones pasan
-    this.presentAlert('Éxito', 'Su cuenta se ha creado exitosamente.');
-    console.log('Formulario válido, proceder con el registro.');
-    this.router.navigate(['/login']);
-  }
-
   dismiss() {
     this.modalController.dismiss();
   }
+
+  async takePicture() { 
+		const image = await Camera.getPhoto({
+			quality: 90,
+			allowEditing: false,
+			resultType: CameraResultType.Uri
+		});
+
+		if (image && image.webPath) { 
+			this.foto_perfil = image.webPath;
+      this.imagen = image.webPath;
+		}
+	}
+
 }
-
-
-
