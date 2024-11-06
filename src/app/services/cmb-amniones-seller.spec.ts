a6043f33ab8e4f8fbe9defb4ca4d0959f502e7a3
vicente <vicerivera.a@gmail.com>
2024-11-05 23:07:10 -0300

    generacion de clases pal combobox

---
+++ src/app/services/cmb-amniones-seller.spec.ts
@@ -0,0 +1,7 @@
+import { CmbAmnionesSeller } from './cmb-amniones-seller';
+
+describe('CmbAmnionesSeller', () => {
+  it('should create an instance', () => {
+    expect(new CmbAmnionesSeller()).toBeTruthy();
+  });
+});
