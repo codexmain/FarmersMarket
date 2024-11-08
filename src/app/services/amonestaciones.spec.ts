64a86b1e02552283de43a6851702dec112bc9e99
vicente <vicerivera.a@gmail.com>
2024-11-08 00:07:52 -0300

    implementacion queries pa hacer amonestaciones y verificar duplicidad de cat y subcat

---
+++ src/app/services/amonestaciones.spec.ts
@@ -0,0 +1,7 @@
+import { Amonestaciones } from './amonestaciones';
+
+describe('Amonestaciones', () => {
+  it('should create an instance', () => {
+    expect(new Amonestaciones()).toBeTruthy();
+  });
+});
---
+++ src/app/services/amonestaciones.ts
@@ -0,0 +1,10 @@
+export class Amonestaciones {
+    id!: number;
+    usuario_id!: number;
+    nombre_usuario?: string;
+    nombre_empresa?: string;
+    id_producto?: any;
+    nombre_producto?: any; //se dejo como any ya que puede haber uno sin producto
+    descripcion!: string;
+
+}
