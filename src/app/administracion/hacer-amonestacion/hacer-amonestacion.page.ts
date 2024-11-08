efc1185d91f0f8a839297661cc7947abaf727c5c
viscerals8 <estigmademuerte@gmail.com>
2024-11-08 01:57:17 -0300

    amonestaciones html
    dwedw

--- src/app/administracion/hacer-amonestacion/hacer-amonestacion.page.ts
+++ src/app/administracion/hacer-amonestacion/hacer-amonestacion.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
+import { AmonestacionesssService } from '../../services/amonestacionesss.service';
 
 @Component({
   selector: 'app-hacer-amonestacion',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hacer-amonestacion.page.scss'],
 })
 export class HacerAmonestacionPage implements OnInit {
+  productos: any[] = [];
 
-  constructor() { }
+  constructor(
+    private modalController: ModalController,
+    private amonestacionesssService: AmonestacionesssService
+  ) { }
 
   ngOnInit() {
+    this.cargarProductos();
   }
 
+  cargarProductos() {
+    this.amonestacionesssService.fetchCmbProdAmnstones().subscribe((data) => {
+      this.productos = data;
+    });
+  }
+
+  enviarAmonestacion(form: NgForm) {
+    if (form.valid) {
+      const { correo, descripcion, idProducto } = form.value;
+      this.amonestacionesssService.enviarAmonestacion(correo, descripcion, idProducto)
+        .then(() => {
+          console.log('Amonestación enviada');
+          this.dismissModal(true); // Cierra el modal con éxito
+          form.reset(); // Reinicia el formulario
+        })
+        .catch((error) => {
+          console.error('Error al enviar amonestación:', error);
+        });
+    }
+  }
+
+  dismissModal(success = false) {
+    this.modalController.dismiss({
+      success: success
+    });
+  }
 }
