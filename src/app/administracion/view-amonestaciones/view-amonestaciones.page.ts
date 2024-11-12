06188a741446c435774699acc9ab33d96f162a37
vicente <vicerivera.a@gmail.com>
2024-11-12 00:42:10 -0300

    visualizar amonestaciones listo

--- src/app/administracion/view-amonestaciones/view-amonestaciones.page.ts
+++ src/app/administracion/view-amonestaciones/view-amonestaciones.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Amonestaciones } from 'src/app/services/amonestaciones';
+import { DataBaseService } from 'src/app/services/data-base.service';
+
 
 @Component({
   selector: 'app-view-amonestaciones',
@@ -6,10 +9,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-amonestaciones.page.scss'],
 })
 export class ViewAmonestacionesPage implements OnInit {
+  searchTerm: string = '';
 
-  constructor() { }
+  arregloAmonestaciones: any = [
+    {
+      id: '',
+      usuario_id: '',
+      nombre_usuario: '',
+      nombre_empresa: '', 
+      id_producto: '',
+      nombre_producto: '',
+      descripcion: ''
+      
+    }
+  ]; 
+
+  filteredAmonestaciones: any = [
+    {
+      id: '',
+      usuario_id: '',
+      nombre_usuario: '',
+      nombre_empresa: '', 
+      id_producto: '',
+      nombre_producto: '',
+      descripcion: ''
+      
+    }
+  ];
+
+  constructor(
+    private bd: DataBaseService,
+  ) { }
 
   ngOnInit() {
+
+    this.bd.dbState().subscribe(data => {
+      if (data) {
+        this.bd.fetchAmonestaciones().subscribe(res => {
+          this.arregloAmonestaciones = res;
+          this.filteredAmonestaciones = res;
+        });
+      }
+    });
+  }
+
+  searchAmonestaciones() {
+    if (this.searchTerm.trim() === '') {
+      this.filteredAmonestaciones = this.arregloAmonestaciones;
+    } else {
+      this.filteredAmonestaciones = this.arregloAmonestaciones.filter((amonestacion: Amonestaciones) =>
+        amonestacion.nombre_usuario.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
   }
 
 }
--- src/app/services/amonestaciones.ts
+++ src/app/services/amonestaciones.ts
@@ -1,7 +1,7 @@
 export class Amonestaciones {
     id!: number;
     usuario_id!: number;
-    nombre_usuario?: string;
+    nombre_usuario!: string;
     nombre_empresa?: string;
     id_producto?: any;
     nombre_producto?: any; //se dejo como any ya que puede haber uno sin producto
