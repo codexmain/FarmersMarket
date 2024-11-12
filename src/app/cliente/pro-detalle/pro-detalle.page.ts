7484553066e62e911ab6daa746bea2a3979d20b1
Ignacio Fuenzalida <i.fuenzalida@duocuc.cl>
2024-11-12 01:24:38 -0300

    ARREGLO PRODUCTO Y CARRITO
    ARREGLOS GENERALES DE VISTA PRODUCTO Y CARRITO, AUN FALTA POR REVISAR ERRORES VARIOS QUE PUEDA TENER

--- src/app/cliente/pro-detalle/pro-detalle.page.ts
+++ src/app/cliente/pro-detalle/pro-detalle.page.ts
@@ -45,6 +45,20 @@ export class ProDetallePage implements OnInit {
     this.subtotal = this.producto.precio * this.cantidad; // Calcular subtotal
   }
 
+  incrementarCantidad() {
+    if (this.cantidad < this.producto.stock) {
+      this.cantidad++;
+      this.actualizarSubtotal();
+    }
+  }
+
+  decrementarCantidad() {
+    if (this.cantidad > 1) {
+      this.cantidad--;
+      this.actualizarSubtotal();
+    }
+  }
+
   async agregarAlCarrito() {
     // Obtener o crear carro de compra
     const carro = await this.dbService.getCarroCompra(this.usuarioId);
