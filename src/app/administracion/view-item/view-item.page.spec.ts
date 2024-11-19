e79ebdcf9512e58c8df703eab26ffd3caed217e4
Ignacio Fuenzalida <i.fuenzalida@duocuc.cl>
2024-11-19 16:03:21 -0300

    Revert "Dos paginas"
    This reverts commit a81e1401038b9027474fe449c5e1d65cddc76349.

--- src/app/administracion/view-item/view-item.page.spec.ts
+++ src/app/administracion/view-item/view-item.page.spec.ts
@@ -1,40 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ViewItemPage } from './view-item.page';
-import { ModalController, NavParams } from '@ionic/angular'; // Importa ModalController y NavParams
-import { IonicModule } from '@ionic/angular'; // Importa IonicModule
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; // Provee HttpClient con interceptores
-import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx'; // Importa SQLite
-import { FormsModule } from '@angular/forms'; // Importa FormsModule
+import { ModalController } from '@ionic/angular'; // Importar ModalController
 
 describe('ViewItemPage', () => {
   let component: ViewItemPage;
   let fixture: ComponentFixture<ViewItemPage>;
   let modalControllerMock: jasmine.SpyObj<ModalController>;
-  let navParamsMock: jasmine.SpyObj<NavParams>;
-  let sqliteMock: jasmine.SpyObj<SQLite>;
 
   beforeEach(async () => {
     // Crear un mock para ModalController
     modalControllerMock = jasmine.createSpyObj('ModalController', ['create', 'dismiss', 'present']);
 
-    // Crear un mock para NavParams
-    navParamsMock = jasmine.createSpyObj('NavParams', ['get']);
-    navParamsMock.get.and.returnValue('mockValue'); // Mock del método `get`
-
-    // Crear un mock para SQLite
-    sqliteMock = jasmine.createSpyObj('SQLite', ['create']);
-
     await TestBed.configureTestingModule({
       declarations: [ViewItemPage],
-      imports: [
-        IonicModule.forRoot(), // Proveer IonicModule para componentes de Ionic
-        FormsModule, // Proveer FormsModule para manejar [(ngModel)]
-      ],
       providers: [
-        provideHttpClient(withInterceptorsFromDi()), // Proveer HttpClient con soporte para interceptores
         { provide: ModalController, useValue: modalControllerMock }, // Proveer el mock de ModalController
-        { provide: NavParams, useValue: navParamsMock }, // Proveer el mock de NavParams
-        { provide: SQLite, useValue: sqliteMock }, // Proveer el mock de SQLite
       ],
     }).compileComponents();
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,23 +9,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+//import { RecuperarPasswordPage } from './users/recuperar-password/recuperar-password.page';
 
-import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx'; // SQL
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx'; //sql
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-  ],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideAnimationsAsync(),
-    SQLite,
-    NativeStorage,
-    provideHttpClient(withInterceptorsFromDi()), // Configurar HttpClient con soporte para interceptores
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync(),SQLite, NativeStorage, provideHttpClient(withInterceptorsFromDi())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
