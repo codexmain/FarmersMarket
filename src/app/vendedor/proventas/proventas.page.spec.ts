3c9e0d807b65b92356d56fb68e9377d9b9123197
Ignacio Fuenzalida <i.fuenzalida@duocuc.cl>
2024-11-19 21:56:44 -0300

    lol

--- src/app/vendedor/proventas/proventas.page.spec.ts
+++ src/app/vendedor/proventas/proventas.page.spec.ts
@@ -4,6 +4,16 @@ import { IonicModule } from '@ionic/angular';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx'; // SQLite
+
+// Mock para SQLite
+class MockSQLite {
+  create() {
+    return Promise.resolve({
+      executeSql: () => Promise.resolve({ rows: { length: 0, item: () => null } }),
+    });
+  }
+}
 
 describe('ProventasPage', () => {
   let component: ProventasPage;
@@ -20,6 +30,7 @@ describe('ProventasPage', () => {
       providers: [
         provideHttpClient(withInterceptorsFromDi()), // Configuración de HttpClient
         provideRouter([]), // Configuración moderna de rutas vacías para pruebas
+        { provide: SQLite, useClass: MockSQLite }, // Mock para SQLite
       ],
     }).compileComponents();
 
